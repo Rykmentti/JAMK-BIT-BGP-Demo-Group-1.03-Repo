77b78acd4f135082ddb9cc8fd1d60c4ca5005c97
Rykmentti <Razorworks991@hotmail.com>
2022-09-25 22:40:44 +0300

    Shoot Projectile commit

---
+++ BGP Proto Group Project/Assets/Contributors/Sami/PlayerProjectile.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProjectile : MonoBehaviour
+    //This script will give speed to the arrow and decide how much damage it does.
+    //Should be attached to the projectile. It should be already attached.
+{
+    public float damage;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        damage = 0; //Insert amount of damage here. If you want to test stuff, leave damage at zero.
+        speed = 10; //How fast the arrow travels.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This moves the projectile forwards.
+        transform.Translate(new Vector2(0f, 1f) * speed * Time.deltaTime);
+    }
+    //Collision Detector
+    void OnTriggerEnter2D(Collider2D collision) //Change identifier to whatever you want. We want to hit the collider, that is around the sprite/object to simulate a "hit".
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            //collision.gameObject.GetComponent</*Name of the script, that is attached to player*/>()./*variable that represents health*/ -= damage; //Insert needed identifiers within */ and /* after wards remove wrappers.
+            //Destroy(gameObject); //Or just destroy the object outright.
+        }
+    }
+}
---
+++ BGP Proto Group Project/Assets/Contributors/Sami/ShootProjectileScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootProjectileScript : MonoBehaviour
+    //Shoot Projectile Script, attach this script to player.
+    //Very simple script, clicking left mouse button will instantiate and turn the gameObject towards where your mouse pointer is. 
+{
+    // Put these values in Unity Editor. playerCamera is the player Camera object and playerArrow is the player arrow prefab, which is in my own folder.
+    public Camera playerCamera;
+    public GameObject playerArrow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShootProjectile();
+    }
+    void ShootProjectile()
+    {
+        //Okay this is a bit more complicated. This calculates position between your mouse in the unity world space and player position, calculates
+        //angle between them and then turns it into quaternion, so we can spawn Game Object in correct angle, so it points towards the mouse pointer.
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            Vector2 mouseScreenPosition = playerCamera.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, playerCamera.nearClipPlane));
+
+            float mousePosX = mouseScreenPosition.x;
+            float mousePosY = mouseScreenPosition.y;
+            float playerPosX = GetComponentInParent<Transform>().position.x;
+            float playerPosY = GetComponentInParent<Transform>().position.y;
+
+            Vector2 Point_1 = new Vector2(mousePosX, mousePosY);
+            Vector2 Point_2 = new Vector2(playerPosX, playerPosY);
+            float rotation = Mathf.Atan2(Point_2.y - Point_1.y, Point_2.x - Point_1.x) * Mathf.Rad2Deg;
+            Vector3 projectileStartRotation = new Vector3(0f, 0f, rotation + 90);
+            Quaternion quaternion = Quaternion.Euler(projectileStartRotation);
+
+            Instantiate(playerArrow, transform.position, quaternion);
+        }
+    }
+}
