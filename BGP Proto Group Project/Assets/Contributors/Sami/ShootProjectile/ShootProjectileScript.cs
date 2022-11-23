36ff630bb02cf41cdc79017c45ab6ca57ec3bc67
Rykmentti <Razorworks991@hotmail.com>
2022-11-23 12:05:51 +0200

    Update ShootProjectileScript.cs
    updated shootProjectile. It now has clip size and holding shoot button shoots projectiles automatically, depending on the firerate.

--- BGP Proto Group Project/Assets/Contributors/Sami/ShootProjectile/ShootProjectileScript.cs
+++ BGP Proto Group Project/Assets/Contributors/Sami/ShootProjectile/ShootProjectileScript.cs
@@ -7,46 +7,79 @@ public class ShootProjectileScript : MonoBehaviour
     //Very simple script, clicking left mouse button will instantiate and turn the gameObject towards where your mouse pointer is. 
 {
     // Put these values in Unity Editor. playerCamera is the player Camera object and playerArrow is the player arrow prefab, which is in my own folder.
-    [SerializeField]
-    Camera playerCamera;
-    [SerializeField]
-    GameObject playerArrow;
+    [SerializeField] Camera playerCamera;
+    [SerializeField] GameObject playerArrow;
+
+    [SerializeField] float firerate;
+    int maxClipSize;
+    int currentClipSize;
+
+    bool shootCooldown;
+    bool isReloading;
     // Start is called before the first frame update
     void OnGUI()
     {
         GUI.Label(new Rect(10, 10, 1000, 30), "Press Left Click to Shoot!"); //Comment this out if you don't want to see text on scene.
+        GUI.Label(new Rect(10, 30, 1000, 30), "Press R to reload, when out of ammo");
+        GUI.Label(new Rect(500, 10, 200, 30), "Ammunition left : " + currentClipSize);
+        if (currentClipSize == 0)
+        {
+            GUI.Label(new Rect(500, 30, 200, 30), "Out of Ammo!");
+        }
     }
     void Start()
     {
-        
+        firerate = 0.15f; // firerate per second.
+        currentClipSize = 30;
+        maxClipSize = 30;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        ShootProjectile();
+        if (Input.GetKey(KeyCode.Mouse0) && shootCooldown == false && isReloading == false && currentClipSize != 0 )
+        {
+            ShootProjectile();
+            currentClipSize -= 1;
+            StartCoroutine(ShootCooldown());
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && isReloading == false)
+        {
+            StartCoroutine(IsReloading());
+        }
     }
     void ShootProjectile()
     {
         //Okay this is a bit more complicated. This calculates position between your mouse in the unity world space and player position, calculates
         //angle between them and then turns it into quaternion, so we can spawn Game Object in correct angle, so it points towards the mouse pointer.
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            Vector3 mousePos = Input.mousePosition;
-            Vector2 mouseScreenPosition = playerCamera.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, playerCamera.nearClipPlane));
+        Vector3 mousePos = Input.mousePosition;
+        Vector2 mouseScreenPosition = playerCamera.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, playerCamera.nearClipPlane));
 
-            float mousePosX = mouseScreenPosition.x;
-            float mousePosY = mouseScreenPosition.y;
-            float playerPosX = GetComponentInParent<Transform>().position.x;
-            float playerPosY = GetComponentInParent<Transform>().position.y;
+        float mousePosX = mouseScreenPosition.x;
+        float mousePosY = mouseScreenPosition.y;
+        float playerPosX = GetComponentInParent<Transform>().position.x;
+        float playerPosY = GetComponentInParent<Transform>().position.y;
 
-            Vector2 Point_1 = new Vector2(mousePosX, mousePosY);
-            Vector2 Point_2 = new Vector2(playerPosX, playerPosY);
-            float rotation = Mathf.Atan2(Point_2.y - Point_1.y, Point_2.x - Point_1.x) * Mathf.Rad2Deg;
-            Vector3 projectileStartRotation = new Vector3(0f, 0f, rotation + 90);
-            Quaternion quaternion = Quaternion.Euler(projectileStartRotation);
+        Vector2 Point_1 = new Vector2(mousePosX, mousePosY);
+        Vector2 Point_2 = new Vector2(playerPosX, playerPosY);
+        float rotation = Mathf.Atan2(Point_2.y - Point_1.y, Point_2.x - Point_1.x) * Mathf.Rad2Deg;
+        Vector3 projectileStartRotation = new Vector3(0f, 0f, rotation + 90);
+        Quaternion quaternion = Quaternion.Euler(projectileStartRotation);
 
-            Instantiate(playerArrow, transform.position, quaternion);
-        }
+        Instantiate(playerArrow, transform.position, quaternion);
+    }
+    IEnumerator IsReloading()
+    {
+        isReloading = true;
+        yield return new WaitForSeconds(2f);
+        currentClipSize = maxClipSize;
+        isReloading = false;
+    }
+    IEnumerator ShootCooldown()
+    {
+        shootCooldown = true;
+        yield return new WaitForSeconds(firerate);
+        shootCooldown = false;
     }
 }
