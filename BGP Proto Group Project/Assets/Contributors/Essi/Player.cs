d4d85e21c89c97957f4ab5e935bed3a145377172
Rykmentti <Razorworks991@hotmail.com>
2022-12-02 19:53:19 +0200

    Update Player.cs

--- BGP Proto Group Project/Assets/Contributors/Essi/Player.cs
+++ BGP Proto Group Project/Assets/Contributors/Essi/Player.cs
@@ -20,10 +20,28 @@ public class Player : MonoBehaviour
     void Update()
     {
         // Gives a value between -1 and 1
-        horizontal = Input.GetAxisRaw("Horizontal"); // -1 is left
-        vertical = Input.GetAxisRaw("Vertical"); // -1 is down
-    }
+        //horizontal = Input.GetAxisRaw("Horizontal"); // -1 is left *Sami* Disabled temporarily
+        //vertical = Input.GetAxisRaw("Vertical"); // -1 is down *Sami Disabled temporarily
 
+        //Imported new control scheme, to test out if works on Essi's end.
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * runSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector2.down * runSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+           transform.Translate(Vector2.left * runSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * runSpeed * Time.deltaTime);
+        }
+    }
+    /* *Sami* Disabled temporarily
     void FixedUpdate()
     {
         if (horizontal != 0 && vertical != 0) // Check for diagonal movement
@@ -35,4 +53,5 @@ public class Player : MonoBehaviour
 
         body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
     }
+    */
 }
